64d08affb7d65b9717eff039dbfc31085a7e14f7
Lukas Plank <lupl@tuta.io>
2024-04-05 12:10:09 +0200

    Added clscor namespace

--- clisn/namespaces.py
+++ clisn/namespaces.py
@@ -8,8 +8,8 @@ from rdflib import Graph, Namespace
 # namespaces
 crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
 crmcls = Namespace("https://clscor.io/ontologies/CRMcls/")
-# clst = Namespace("https://core.clscor.io/entity/type/")
-# clscore = Namespace("https://core.clscor.io/entity/")
+clscore = Namespace("https://clscor.io/entity/")
+
 frbroo = Namespace("http://iflastandards.info/ns/fr/frbr/frbroo/")
 crmdig = Namespace("http://www.ics.forth.gr/isl/CRMdig/")
 lrm = Namespace("http://iflastandards.info/ns/lrm/lrmoo/")
